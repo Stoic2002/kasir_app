d274682ab40c328e45a18071f2e534a4344bebe7
Arul Karim <lucknoexcuse15@gmail.com>
2024-06-05 19:38:52 +0700

    new

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color color01 = Color(0xFF6D57FC);
+
+  static const Color color02 = Color(0xFF261E58);
+
+  static const Color color03 = Color(0xFF0C0A1C);
+
+  static const Color color04 = Color(0xFFE8E4FF);
+
+  static const Color color05 = Color(0xFFB0A4FD);
+}
